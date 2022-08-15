98de7b7b710454f3c5297efd9f04acac8b1b97e8
Harsha Revanth Manchikanti <90206268+Harsha2306@users.noreply.github.com>
2022-08-15 08:57:23 +0530

    Update conn.java

--- bank-management-system/src/conn.java
+++ bank-management-system/src/conn.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+
 /*
 1)Register the Driver class
 2)Create connection
@@ -16,8 +17,8 @@ public class conn {
    conn(){
        try {
            Class.forName("com.mysql.cj.jdbc.Driver");
-           c = DriverManager.getConnection(url,user,password);
-           s = c.createStatement();
+           c = DriverManager.getConnection(url,user,password);//The getConnection() method of DriverManager class is used to establish connection with the database
+           s = c.createStatement();//The createStatement() method of Connection interface is used to create statement
        }
        catch (Exception e){
            e.printStackTrace();
